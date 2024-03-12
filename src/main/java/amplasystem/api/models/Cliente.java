c3fe19a5deba0e893835d0701e2f8088205f8682
Pedro Henrique Pires Rodrigues <67903358+Credosvaldo@users.noreply.github.com>
2024-03-12 09:08:49 -0300

    fix: anotations das colunas adicionadas nos atributos dos models

--- src/main/java/amplasystem/api/models/Cliente.java
+++ src/main/java/amplasystem/api/models/Cliente.java
@@ -2,12 +2,15 @@ package amplasystem.api.models;
 
 import java.util.List;
 
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,17 +21,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Cliente {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String cnpj;
-    private String telefone;
-    private String cidade;
-    private String endereco;
+
+    @Column(name = "nome_fantasia", nullable = false, length = 100)
+    @NotBlank(message = "Nome fantasia do cliente é obrigatorio")
     private String nomeFantasia;
+
+
+
 
     @ManyToOne
     @JoinColumn(name = "vendedor_id")
     private Vendedor vendedor;
-
 
     @OneToMany(mappedBy = "cliente")
     private List<OrdemDeCompra> ordemDeCompras;
--- src/main/java/amplasystem/api/models/Financeiro.java
+++ src/main/java/amplasystem/api/models/Financeiro.java
@@ -1,9 +1,15 @@
 package amplasystem.api.models;
 
+import amplasystem.api.enuns.Faturamento;
+import amplasystem.api.enuns.TipoFiscal;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,10 +20,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Financeiro {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "comissao", nullable = false)
     private Double comissao;
-    private String faturamento;
-    private String tipoFiscal;
+
+    @Column(name = "faturamento", nullable = false)
+    private Faturamento faturamento;
+
+    @Column(name = "tipo_fiscal", nullable = false)
+    private TipoFiscal tipoFiscal;
     
     @OneToOne
     @JoinColumn(name = "industria_id")
